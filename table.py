80b04c18309680f57d76fb56ec8015fbe8b44888
Jayaraj <jayaraj.sivadasan@gmail.com>
2022-04-07 18:07:52 -0400

    Exception handling added

--- table.py
+++ table.py
@@ -1,12 +1,18 @@
-number = int(input ('Which number do you want the table for?'))
-limit = int(input('Limit?'))
-counter=1
-print ('Printing multiplication table of ' + str(number) + ' up to ' +str(limit) )
-maxlength = len(str(number*limit))
-while counter<=limit: 
+try:
+    number = int(input ('Which number do you want the table for?'))
+    limit = int(input('Limit?'))
+    counter=1
+    print ('Printing multiplication table of ' + str(number) + ' up to ' +str(limit) )
+    maxlength = len(str(number*limit))
+    while counter<=limit: 
+
+        print (str(number).rjust(len(str(number)),' ')+' x ' + str(counter).rjust(len(str(limit)),' ') + ' = ' + str(number*counter).rjust(maxlength,' '))
+        counter+=1   # counter= counter +1 
+
+except: 
+    print ('Input is not a valid number')
     
-    print (str(number).rjust(len(str(number)),' ')+' x ' + str(counter).rjust(len(str(limit)),' ') + ' = ' + str(number*counter).rjust(maxlength,' '))
-    counter+=1   # counter= counter +1 
+
 
 
    
