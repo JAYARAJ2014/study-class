32f856f675caaaee9e6adfd83d5dae14559c100f
Jayaraj <jayaraj.sivadasan@gmail.com>
2022-04-07 17:54:04 -0400

    equalize the spacing

--- table.py
+++ table.py
@@ -2,10 +2,11 @@ number = int(input ('Which number do you want the table for?'))
 limit = int(input('Limit?'))
 counter=1
 print ('Printing multiplication table of ' + str(number) + ' up to ' +str(limit) )
+maxlength = len(str(number*limit))
 while counter<=limit: 
-    print (str(number)+' x ' + str(counter) + ' = ' + str(number*counter))
+    print (str(number).rjust(maxlength,' ')+' x ' + str(counter).rjust(maxlength,' ') + ' = ' + str(number*counter).rjust(maxlength,' '))
     counter+=1   # counter= counter +1 
 
 
-    ### Future improvement opportunity: equalize the spacing
+   
     ### Validate input and handle gracefully
