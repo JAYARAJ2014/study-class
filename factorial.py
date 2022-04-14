9e00b72dfae517507c28bbb4c2a19952576c559d
Jayaraj <jayaraj.sivadasan@gmail.com>
2022-04-13 21:13:51 -0400

    updated with more comments

--- factorial.py
+++ factorial.py
@@ -1,12 +1,18 @@
-# This program prints HELLO 100 times
+ 
+# Accept a number from the console  / terminal
 
+# the user keys in a number. We are converting it to integer and
+# storing it in a variable named number 
 number = int(input ('Please enter a number '))
+# initialize a counter to 1 
 counter = 1
-fact = 1;
+# Initialized as one because, factorial of zero and one is equal to 1
+factorial = 1; ## accumulator variable
 
 while counter<=number: 
-    fact *= counter
+    # factorial = factorial * counter
+    factorial *=  counter
     counter+=1   # counter= counter +1 
 
 
-print (number,'!=',fact)
+print (number,'!=',factorial)
