5b1d6aad77c63251fcf0ce402c3e161159578990
Jayaraj <jayaraj.sivadasan@gmail.com>
2022-04-13 21:06:06 -0400

    updates

---
+++ for.py
@@ -0,0 +1,36 @@
+
+# #Function definition
+# Function parameter
+# there can be a return value
+# Calling the function / Function invocation
+
+
+def hello(name):
+    print("Hello ", name)
+
+
+# #Function invocation
+hello('Priya')
+hello('Saithya')
+
+
+#function defintion
+# in parenthesis we provide the parameter
+def square (n):
+    return n*n
+
+# # y gets  value that is returned from the function square
+y = square(9)
+print(y)
+
+
+# y = square(1.4)
+# print (y)
+
+
+def area(length,width):
+    return length * width 
+
+
+y = area (5,5)
+print(y)
--- sumofn.py
+++ sumofn.py
@@ -1,10 +1,12 @@
 # This program prints Sum of 1 to 100
 
 counter = 1
-sum = 0
-while counter<=100: 
-    sum+=counter 
+sum = 0 # accumulator variable.
+while counter<=500: 
+    # print (counter)
+    sum = sum + counter 
     counter+=1   # counter= counter +1 
+
 
 print (sum)
 
